d5a14d03498f71a676927a5e7eb70bfcc9843855
mayankjx <prototype.mayank@gmail.com>
2023-03-06 18:58:25 +0530

    - Added location API to mock server - Loaded ideal JSON responsed to mock server database

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,33 +1,59 @@
-import { createServer } from "miragejs";
+import { createServer, Model, Response } from "miragejs";
 
 export function makeServer({ environment = "development" } = {}) {
   let server = createServer({
     environment,
 
+    models: {
+      location: Model,
+    },
+
     routes() {
       this.namespace = "api";
 
-      this.get("/location", () => {
-        return [
-          {
-            location_id: 1,
-            title: "Hall - 3",
-            description: "Boys hall of residence",
-          },
-          {
-            location_id: 2,
-            title: "Hall - 1",
-            description: "Boys hall of residence",
-          },
-          {
-            location_id: 3,
-            title: "Hall - 4",
-            description: "Boys hall of residence",
-          },
-        ];
+      this.get("/location", (schema) => {
+        return schema.locations.all();
       });
+
+      this.post("/location", (schema, request) => {
+        let body = JSON.parse(request.requestBody);
+        let success = schema.locations.create(body);
+        if (success.attrs) {
+          return new Response(200, { some: "OK" });
+        }
+        return new Response(400, { errors: ["Some error occured"] });
+      });
+
+      this.get("/location/searchId/:locationId", (schema, request) => {
+        return schema.locations.find(request.params.locationId);
+      });
+
+      this.del("/location/searchId/:locationId", (schema, request) => {
+        return schema.locations.find(request.params.locationId).destroy();
+      });
+
+      // search by name is not implemented in mock server because of its complex nature
     },
   });
-
+  // loading mock data onto the server
+  server.db.loadData({
+    locations: [
+      {
+        location_id: 1,
+        title: "Hall 3",
+        description: "Boys hall of residence",
+      },
+      {
+        location_id: 2,
+        title: "LHTC",
+        description: "Central building of campus having all the classrooms",
+      },
+      {
+        location_id: 3,
+        title: "PHC",
+        description: "Hospital and pharamcy of campus",
+      },
+    ],
+  });
   return server;
 }
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -9,8 +9,46 @@ import axios from "axios";
 export default {
   name: "Home",
   mounted() {
+    // get all the locations
     axios.get("/api/location").then((response) => {
-      console.log(response.data);
+      console.log(response.data, response.status);
+    });
+
+    let newLocation = {
+      location_id: 4,
+      title: "OAT",
+      description: "Open Air Theatre where varity of events happen",
+    };
+
+    // creating a new location
+    axios
+      .post("/api/location", newLocation)
+      .then((response) => {
+        console.log(response.status);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    // searching a location by location id
+    let locationId = 1;
+    axios
+      .get(`/api/location/searchId/${locationId}`)
+      .then((response) => {
+        console.log(response.data, response.status);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    // deleting a location by location id
+    axios.delete(`/api/location/searchId/${locationId}`).then((response) => {
+      console.log(response.data, response.status);
+    });
+
+    // getting all location again
+    axios.get("/api/location").then((response) => {
+      console.log(response.data, response.status);
     });
   },
 };
