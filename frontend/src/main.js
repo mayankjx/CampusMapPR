9dd4140853a9c19f1c3dc380db8322cc3c11d3f8
mayankjx <prototype.mayank@gmail.com>
2023-03-14 12:01:22 +0530

    added chakra UI

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import Chakra, { CThemeProvider, CReset } from "@chakra-ui/vue";
 
-Vue.config.productionTip = false
+Vue.use(Chakra);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(CThemeProvider, [h(CReset), h(App)]),
+}).$mount("#app");
