882e10d8238a6c7107905f700a1b9b96fb4bb22c
mayankjx <prototype.mayank@gmail.com>
2023-03-14 11:25:07 +0530

    Downgraded vue 3 to vue 2 because chakra ui does not support development with vue 3 yet.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,10 @@
-import process from "process";
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import { makeServer } from "./server";
-// import { Server } from "miragejs";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
 
-// comment this out when changing from mock server to real server
-makeServer();
+Vue.config.productionTip = false
 
-createApp(App).use(router).mount("#app");
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,118 +1,18 @@
 <template>
-  <h1>hey</h1>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import axios from "axios";
+import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: "Home",
-  methods: {
-    locationsApi() {
-      // get all the locations
-      axios.get("/api/location").then((response) => {
-        console.log(response.data, response.status);
-      });
-
-      let newLocation = {
-        location_id: 4,
-        title: "OAT",
-        description: "Open Air Theatre where varity of events happen",
-      };
-
-      // creating a new location
-      axios
-        .post("/api/location", newLocation)
-        .then((response) => {
-          console.log(response.status);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-
-      // searching a location by location id
-      let locationId = 1;
-      axios
-        .get(`/api/location/searchId/${locationId}`)
-        .then((response) => {
-          console.log(response.data, response.status);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-
-      // deleting a location by location id
-      axios.delete(`/api/location/searchId/${locationId}`).then((response) => {
-        console.log(response.data, response.status);
-      });
-
-      // getting all location again
-      axios.get("/api/location").then((response) => {
-        console.log(response.data, response.status);
-      });
-    },
-
-    eventsApi() {
-      // get all the events
-      axios.get("/api/event").then((response) => {
-        console.log(response.status, response.data);
-      });
-
-      let event = {
-        event_id: 4,
-        title: "Random event",
-        description: "Blah blah blah",
-        start_date: "20-05-2023",
-        end_date: "30-05-2023",
-        created_by: 6,
-        location: 14,
-      };
-
-      // create a new event
-      axios
-        .post("/api/event", event)
-        .then((response) => console.log(response.data))
-        .catch((error) => console.log(error));
-
-      axios.get("/api/event").then((response) => {
-        console.log(response.status, response.data);
-      });
-
-      // get event using event ID
-      let eventId = 2;
-      axios
-        .get(`/api/event/searchId/${eventId}`)
-        .then((response) => console.log(response.data));
-
-      // delete event using event ID
-      axios
-        .delete(`/api/event/searchId/${eventId}`)
-        .then((response) => console.log(response.data))
-        .catch((error) => console.log(error));
-
-      axios.get("/api/event").then((response) => {
-        console.log(response.status, response.data);
-      });
-
-      let organizerId = 9;
-
-      // Get all events by Organizer ID => by a particular organizer
-      axios
-        .get(`/api/event/searchOid/${organizerId}`)
-        .then((response) => console.log(response.data));
-
-      let locationId = 19;
-
-      // Get all events by location ID => on a particular location
-      axios
-        .get(`/api/event/searchLid/${locationId}`)
-        .then((response) => console.log(response.data));
-    },
-  },
-  mounted() {
-    // this.locationsApi();
-    this.eventsApi();
-  },
-};
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
 </script>
