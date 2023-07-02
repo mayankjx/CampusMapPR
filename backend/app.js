4919fb8b5fb50a66d5c3ff049d2c9fd72f201bdc
mayankjx <prototype.mayank@gmail.com>
2023-07-02 18:36:03 +0530

    render delpy

--- backend/app.js
+++ backend/app.js
@@ -4,3 +4,12 @@ const { PrismaClient } = require("@prisma/client");
 const app = express();
 
 app.use(express.json());
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Server is up and running!");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
